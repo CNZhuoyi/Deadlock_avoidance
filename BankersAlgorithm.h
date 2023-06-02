72e21e4f071bdfe31f3f12c8352626e8e8de6710
ZhuoYi_HK <zhuoyide@gmail.com>
2023-06-02 21:07:39 +0800

    添加项目文件。

---
+++ BankersAlgorithm.h
@@ -0,0 +1,10 @@
+#pragma once
+#include<iostream>
+
+//int n;//进程数
+//int m;//资源类型数
+//int available[m];//每类资源剩余量
+//int max[n][m];//进程需要每类资源的最大需求量
+//int allocation[n][m];//进程已得到的资源量
+//int need[n][m]; //进程还需要的资源量
+//int request[n][m];//进程申请的资源量
